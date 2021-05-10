938603a72e0c8a62af8d47caec986fe87e6866bc
1753215994@qq.com <1753215994@qq.com>
2021-05-10 15:26:55 +0800

    修复正式包没办法上传图片的问题更新插件image_Picker

--- android/app/src/main/kotlin/com/example/familytest/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/familytest/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.familytest
 
+import android.content.Intent
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
 
 class MainActivity: FlutterActivity() {
+
 }
