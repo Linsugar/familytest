866632c3333be822ebfa2ba5640f80ce053f065e
tangtang <1753215994@qq.com>
2021-05-12 07:48:48 +0800

    更新评论界面

--- lib/pages/chat/cpn/reviewcpn.dart
+++ lib/pages/chat/cpn/reviewcpn.dart
@@ -46,109 +46,203 @@ class _reviewCpnState extends State<reviewCpn> {
     return Scaffold(
       resizeToAvoidBottomInset: true,
       appBar: AppBar(title: Text("详情"),),
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Flex(
-            direction: Axis.vertical,
-            children: [
-              Expanded(flex: 3,child:Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ListTile(
-                      leading: CircleAvatar(backgroundImage: NetworkImage(data.avator),),
-                      title: Text("${data.title}"),
-                      subtitle:  Text("发布时间:${data.time}"),
-                    ),
-                    Container(
-                        margin: EdgeInsets.only(left: 5),
-                        constraints: BoxConstraints(
-                          maxHeight: 100,
-                          minHeight: 10,
-                        ),
-                        child:  Text("${data.con}",textAlign: TextAlign.start,maxLines: 3,overflow:TextOverflow.ellipsis)
-                    ),
-                    SizedBox(height: 10),
-                    Container(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            Expanded(flex:5,child:Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ListTile(
+                    leading: CircleAvatar(backgroundImage: NetworkImage(data.avator),),
+                    title: Text("${data.title}"),
+                    subtitle:  Text("发布时间:${data.time}"),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(left: 5),
                       constraints: BoxConstraints(
-                        minHeight: 50,
                         maxHeight: 100,
+                        minHeight: 30,
                       ),
-                      child:Column(
-                        children: [
-                          Expanded(flex: 3,
-                            child:_imagelist.isEmpty?Text("用户未上传图片"):ListView.separated(
-                                scrollDirection: Axis.horizontal,
-                                itemBuilder: (context,im){
-                                  return Container(
-                                    margin: EdgeInsets.only(left: 5),
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(5),
-                                        image: DecorationImage(
-                                            image: NetworkImage(_imagelist[im]),
-                                            fit: BoxFit.cover
-                                        )
-                                    ),
-                                    width: 80,
-                                  );
-                                }, separatorBuilder: (context,im){
-                              return SizedBox(width: 5,height: 5,);
-                            }, itemCount:_imagelist.length),
-                          ),
-                        ],
-                      ) ,
-                    )
-                  ],
-                ),
-              )),
-              Expanded(child:Row(children: [SizedBox(width: 5,),Text("评论",textAlign:TextAlign.start,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w600),),],),),
-              Expanded(flex: 4,child: FutureBuilder(
-                future: _getreview(data.dyid),
-                builder: (BuildContext context, AsyncSnapshot snapshot){
-                  if(snapshot.connectionState==ConnectionState.waiting){
-                    return Center(child: CircularProgressIndicator(),);
-                  }if(snapshot.hasData){
-                    List snda = snapshot.data;
-                    if(snda.isEmpty){
-                      return Center(child: Text("当前暂无评论"),);
-                    }else{
-                      return ListView.separated(itemBuilder: (context,index){
-                        return ListTile(
-                          leading: CircleAvatar(backgroundImage: NetworkImage(snda[index]['recview_avator']),),
-                          title: Text("${snda[index]['review_name']}"),
-                          subtitle: Text("${snda[index]['review_content']}"),
-                        );
-                      }, separatorBuilder: (context,index){
-                        return Divider();
-                      }, itemCount: snda.length);
-                    }
-                  }
-                  else{
-                    return Text("数据有误");
-                  }
-                },
-              )),
-              Expanded(child:Flex(direction: Axis.horizontal,children: [
-                Expanded(flex: 7,child:  Container(
-                  child: TextField(controller: _textEditingController,),
-                )),
-                Expanded(flex: 3,child: MaterialButton(child: Text("发送"),onPressed: ()async{
-                 await _postreview(data.dyid);
-                 _textEditingController.clear();
-                 PopupUntil.showToast("发布成功,请刷新页面");
-                 setState(() {
-
-                 });
-                },))
-              ],),),
-            ],
-          ),
+                      child:  Text("${data.con}",textAlign: TextAlign.start,maxLines: 3,overflow:TextOverflow.ellipsis)
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    constraints: BoxConstraints(
+                      minHeight: 50,
+                      maxHeight: 100,
+                    ),
+                    child: Container(
+                      height: 100,
+                      child: _imagelist.isEmpty?Text("用户未上传图片"):ListView.separated(
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context,im){
+                            return Container(
+                              margin: EdgeInsets.only(left: 5),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  image: DecorationImage(
+                                      image: NetworkImage(_imagelist[im]),
+                                      fit: BoxFit.cover
+                                  )
+                              ),
+                              width: 80,
+                            );
+                          }, separatorBuilder: (context,im){
+                        return SizedBox(width: 5,height: 5,);
+                      }, itemCount:_imagelist.length),
+                    ),
+                  ),
+                  Row(children: [SizedBox(width: 5,),Text("评论",textAlign:TextAlign.start,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w600),),],),
+                  Expanded(flex: 5,child: FutureBuilder(
+                    future: _getreview(data.dyid),
+                    builder: (BuildContext context, AsyncSnapshot snapshot){
+                      if(snapshot.connectionState==ConnectionState.waiting){
+                        return Center(child: CircularProgressIndicator(),);
+                      }if(snapshot.hasData){
+                        List snda = snapshot.data;
+                        if(snda.isEmpty){
+                          return Center(child: Text("当前暂无评论"),);
+                        }else{
+                          return ListView.separated(itemBuilder: (context,index){
+                            return ListTile(
+                              leading: CircleAvatar(backgroundImage: NetworkImage(snda[index]['recview_avator']),),
+                              title: Text("${snda[index]['review_name']}"),
+                              subtitle: Text("${snda[index]['review_content']}"),
+                            );
+                          }, separatorBuilder: (context,index){
+                            return Divider();
+                          }, itemCount: snda.length);
+                        }
+                      }
+                      else{
+                        return Text("数据有误");
+                      }
+                    },
+                  )),
+                  Row(
+                    children: [
+                      Expanded(flex: 7,child:  Container(
+                        child: TextField(controller: _textEditingController,maxLines: 1,
+                          decoration: InputDecoration(
+                          hintText: "请输入评论内容",
+                        ),),
+                      )),
+                      Expanded(flex: 3,child: MaterialButton(child: Text("发送"),onPressed: ()async{
+                        await _postreview(data.dyid);
+                        _textEditingController.clear();
+                        PopupUntil.showToast("发布成功,请刷新页面");
+                        setState(() {
+                        });
+                      },))
+                    ],
+                  )
+                ],
+              ),
+            ))
+          ],
         ),
-      ),
+      )
     );
   }
 }
 
 
 
+//Flex(
+//direction: Axis.vertical,
+//children: [
+//Expanded(flex:5,child:Container(
+//child: Column(
+//crossAxisAlignment: CrossAxisAlignment.start,
+//children: [
+//ListTile(
+//leading: CircleAvatar(backgroundImage: NetworkImage(data.avator),),
+//title: Text("${data.title}"),
+//subtitle:  Text("发布时间:${data.time}"),
+//),
+//Container(
+//margin: EdgeInsets.only(left: 5),
+//constraints: BoxConstraints(
+//maxHeight: 100,
+//minHeight: 10,
+//),
+//child:  Text("${data.con}",textAlign: TextAlign.start,maxLines: 3,overflow:TextOverflow.ellipsis)
+//),
+//SizedBox(height: 10),
+//Container(
+//constraints: BoxConstraints(
+//minHeight: 50,
+//maxHeight: 100,
+//),
+//child:Column(
+//children: [
+//Expanded(flex: 3,
+//child:_imagelist.isEmpty?Text("用户未上传图片"):ListView.separated(
+//scrollDirection: Axis.horizontal,
+//itemBuilder: (context,im){
+//return Container(
+//margin: EdgeInsets.only(left: 5),
+//decoration: BoxDecoration(
+//borderRadius: BorderRadius.circular(5),
+//image: DecorationImage(
+//image: NetworkImage(_imagelist[im]),
+//fit: BoxFit.cover
+//)
+//),
+//width: 80,
+//);
+//}, separatorBuilder: (context,im){
+//return SizedBox(width: 5,height: 5,);
+//}, itemCount:_imagelist.length),
+//),
+//],
+//) ,
+//)
+//],
+//),
+//)),
+//Expanded(flex: 5,child: FutureBuilder(
+//future: _getreview(data.dyid),
+//builder: (BuildContext context, AsyncSnapshot snapshot){
+//if(snapshot.connectionState==ConnectionState.waiting){
+//return Center(child: CircularProgressIndicator(),);
+//}if(snapshot.hasData){
+//List snda = snapshot.data;
+//if(snda.isEmpty){
+//return Center(child: Text("当前暂无评论"),);
+//}else{
+//return ListView.separated(itemBuilder: (context,index){
+//return ListTile(
+//leading: CircleAvatar(backgroundImage: NetworkImage(snda[index]['recview_avator']),),
+//title: Text("${snda[index]['review_name']}"),
+//subtitle: Text("${snda[index]['review_content']}"),
+//);
+//}, separatorBuilder: (context,index){
+//return Divider();
+//}, itemCount: snda.length);
+//}
+//}
+//else{
+//return Text("数据有误");
+//}
+//},
+//)),
+//Row(
+//children: [
+//Expanded(flex: 7,child:  Container(
+//child: TextField(controller: _textEditingController,),
+//)),
+//Expanded(flex: 3,child: MaterialButton(child: Text("发送"),onPressed: ()async{
+//await _postreview(data.dyid);
+//_textEditingController.clear();
+//PopupUntil.showToast("发布成功,请刷新页面");
+//setState(() {
+//
+//});
+//},))
+//],
+//)
+//],
+//)
