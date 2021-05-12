c7cf536209f04fcba3c65a9f0c83168ee5353290
tangtang <1753215994@qq.com>
2021-05-12 08:11:56 +0800

    更新评论小部件

--- lib/pages/chat/cpn/reviewcpn.dart
+++ lib/pages/chat/cpn/reviewcpn.dart
@@ -1,4 +1,6 @@
 import 'package:familytest/network/requests.dart';
+import 'package:familytest/pages/home/Home.dart';
+import 'package:familytest/provider/homeState.dart';
 import 'package:familytest/until/showtoast.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -18,12 +20,17 @@ class _reviewCpnState extends State<reviewCpn> {
   void initState() {
     // TODO: implement initState
     data = widget.arguments!['data'];
+    _getreview(data.dyid);
     super.initState();
   }
 
   _getreview(int ?dyid)async{
+    Provider.of<homeState>(context,listen: false).reviewlist.clear();
    var _result = await Request.getNetwork('review/',params: {'review_rd':dyid});
    print("得到的结果数据：$_result");
+   _result.forEach((value){
+     Provider.of<homeState>(context,listen: false).changereview(value);
+   });
    return _result;
   }
 
@@ -35,7 +42,6 @@ class _reviewCpnState extends State<reviewCpn> {
      'review_name':context.read<GlobalState>().username,
      'review_bool':1,
    });
-   print("发布评论结果：$_result");
    return _result;
   }
 
@@ -43,6 +49,8 @@ class _reviewCpnState extends State<reviewCpn> {
   @override
   Widget build(BuildContext context) {
   List  _imagelist = data.imagelist;
+  List snda = context.watch<homeState>().reviewlist;
+  print("数据是否有误：$snda");
     return Scaffold(
       resizeToAvoidBottomInset: true,
       appBar: AppBar(title: Text("详情"),),
@@ -95,32 +103,15 @@ class _reviewCpnState extends State<reviewCpn> {
                     ),
                   ),
                   Row(children: [SizedBox(width: 5,),Text("评论",textAlign:TextAlign.start,style: TextStyle(fontSize: 15,fontWeight: FontWeight.w600),),],),
-                  Expanded(flex: 5,child: FutureBuilder(
-                    future: _getreview(data.dyid),
-                    builder: (BuildContext context, AsyncSnapshot snapshot){
-                      if(snapshot.connectionState==ConnectionState.waiting){
-                        return Center(child: CircularProgressIndicator(),);
-                      }if(snapshot.hasData){
-                        List snda = snapshot.data;
-                        if(snda.isEmpty){
-                          return Center(child: Text("当前暂无评论"),);
-                        }else{
-                          return ListView.separated(itemBuilder: (context,index){
-                            return ListTile(
-                              leading: CircleAvatar(backgroundImage: NetworkImage(snda[index]['recview_avator']),),
-                              title: Text("${snda[index]['review_name']}"),
-                              subtitle: Text("${snda[index]['review_content']}"),
-                            );
-                          }, separatorBuilder: (context,index){
-                            return Divider();
-                          }, itemCount: snda.length);
-                        }
-                      }
-                      else{
-                        return Text("数据有误");
-                      }
-                    },
-                  )),
+                  Expanded(flex: 5,child:snda.isEmpty?Text("暂无评论，快来做第一个吧"):ListView.separated(itemBuilder: (context,index){
+                    return ListTile(
+                      leading: CircleAvatar(backgroundImage: NetworkImage(snda[index]['recview_avator']),),
+                      title: Text("${snda[index]['review_name']}"),
+                      subtitle: Text("${snda[index]['review_content']}"),
+                    );
+                  }, separatorBuilder: (context,index){
+                    return Divider();
+                  }, itemCount: snda.length)),
                   Row(
                     children: [
                       Expanded(flex: 7,child:  Container(
@@ -131,6 +122,7 @@ class _reviewCpnState extends State<reviewCpn> {
                       )),
                       Expanded(flex: 3,child: MaterialButton(child: Text("发送"),onPressed: ()async{
                         await _postreview(data.dyid);
+                        await _getreview(data.dyid);
                         _textEditingController.clear();
                         PopupUntil.showToast("发布成功,请刷新页面");
                         setState(() {
@@ -150,60 +142,7 @@ class _reviewCpnState extends State<reviewCpn> {
 
 
 
-//Flex(
-//direction: Axis.vertical,
-//children: [
-//Expanded(flex:5,child:Container(
-//child: Column(
-//crossAxisAlignment: CrossAxisAlignment.start,
-//children: [
-//ListTile(
-//leading: CircleAvatar(backgroundImage: NetworkImage(data.avator),),
-//title: Text("${data.title}"),
-//subtitle:  Text("发布时间:${data.time}"),
-//),
-//Container(
-//margin: EdgeInsets.only(left: 5),
-//constraints: BoxConstraints(
-//maxHeight: 100,
-//minHeight: 10,
-//),
-//child:  Text("${data.con}",textAlign: TextAlign.start,maxLines: 3,overflow:TextOverflow.ellipsis)
-//),
-//SizedBox(height: 10),
-//Container(
-//constraints: BoxConstraints(
-//minHeight: 50,
-//maxHeight: 100,
-//),
-//child:Column(
-//children: [
-//Expanded(flex: 3,
-//child:_imagelist.isEmpty?Text("用户未上传图片"):ListView.separated(
-//scrollDirection: Axis.horizontal,
-//itemBuilder: (context,im){
-//return Container(
-//margin: EdgeInsets.only(left: 5),
-//decoration: BoxDecoration(
-//borderRadius: BorderRadius.circular(5),
-//image: DecorationImage(
-//image: NetworkImage(_imagelist[im]),
-//fit: BoxFit.cover
-//)
-//),
-//width: 80,
-//);
-//}, separatorBuilder: (context,im){
-//return SizedBox(width: 5,height: 5,);
-//}, itemCount:_imagelist.length),
-//),
-//],
-//) ,
-//)
-//],
-//),
-//)),
-//Expanded(flex: 5,child: FutureBuilder(
+//FutureBuilder(
 //future: _getreview(data.dyid),
 //builder: (BuildContext context, AsyncSnapshot snapshot){
 //if(snapshot.connectionState==ConnectionState.waiting){
@@ -228,21 +167,4 @@ class _reviewCpnState extends State<reviewCpn> {
 //return Text("数据有误");
 //}
 //},
-//)),
-//Row(
-//children: [
-//Expanded(flex: 7,child:  Container(
-//child: TextField(controller: _textEditingController,),
-//)),
-//Expanded(flex: 3,child: MaterialButton(child: Text("发送"),onPressed: ()async{
-//await _postreview(data.dyid);
-//_textEditingController.clear();
-//PopupUntil.showToast("发布成功,请刷新页面");
-//setState(() {
-//
-//});
-//},))
-//],
-//)
-//],
 //)
--- lib/provider/homeState.dart
+++ lib/provider/homeState.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 class homeState with ChangeNotifier {
 String ?hometitle ="哈喽";
 List homelist = [];
+List reviewlist = [];
+
+changereview(value){
+  reviewlist.add(value);
+  notifyListeners();
+}
 
 changelist(value){
   homelist.add(value);
